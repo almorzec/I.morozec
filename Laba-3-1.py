189d3fd580f2350c90d34fa5e289ccdc57b21e4e
almorzec <123204688+almorzec@users.noreply.github.com>
2023-01-25 23:21:37 +0300

    Add files via upload

---
+++ Laba-3-1.py
@@ -0,0 +1,7 @@
+# -- coding: utf-8 --
+from random import random as r
+n,k=map(int,input('n k > ').split())
+k-=1; b=range(n)
+a=[[r() for j in b] for i in b]; print(a)
+for j in b: a[k][j]/=a[k][k]
+print(a)
